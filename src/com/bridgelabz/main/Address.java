5e496a4c79cc1b6845071d4d3239a33202b56853
Tanishka Jain <tanishkajain1508@gmail.com>
2023-12-04 19:13:57 +0530

    First Commit

---
+++ src/com/bridgelabz/main/Address.java
@@ -0,0 +1,154 @@
+package com.bridgelabz.main;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+/*@desc:Class representing the entire address book.
+@params:ArrayList(Contacts)
+@methods:non-parameterized contstructor, display, addContact,findContact, editContact, addContact,deleteContact
+*/
+class Address{
+
+    private ArrayList<Contact> contacts;
+
+    public Address(){
+        contacts = new ArrayList<Contact>();
+    }
+    
+	/*
+	 * @desc: to add individual conatcts in  ArrayList
+	 * @params:Object Contact
+	 * @return:none
+	 */
+    public void addContact(Contact contact) {
+        // Check for duplicates using Java Streams
+        boolean isDuplicate = contacts.stream().anyMatch(c -> c.equals(contact));
+
+        if (!isDuplicate) {
+            contacts.add(contact);
+            System.out.println("Contact added successfully");
+        } else {
+            System.out.println("Duplicate entry. Contact not added.");
+        }
+    }
+    
+    
+	/*
+	 * @desc: to display individual conatcts in  ArrayList
+	 * @params:none
+	 * @return:none
+	 */
+    public void display() {
+        if(contacts.isEmpty()){
+            System.out.println("No contacts in the address book");
+        }else{
+            System.out.println("Address book updated\n");
+            for(Contact c : contacts){
+                System.out.println(c);
+                System.out.println();    
+        }
+        }  
+    }
+    
+	/*
+	 * @desc:Finds a contact in the address book by first name and last name.
+	 * @params:String
+	 * @return:Object
+	 */
+    public Contact findContactByName(String firstName, String lastName) {
+        for (Contact contact : contacts) {
+            if (contact.getFname().equalsIgnoreCase(firstName) && contact.getLname().equalsIgnoreCase(lastName)) {
+                return contact;
+            }
+        }
+        return null; // Contact not found
+    }
+   
+	/*
+	 * @desc:Edits an existing contact's information based on user input.
+	 * @params:Scanner
+	 * @return:none
+	 */
+    public void editExistingContact(Scanner scanner) {
+        System.out.print("Enter the first name of the contact to edit: ");
+        String firstNameToEdit = scanner.nextLine();
+
+        System.out.print("Enter the last name of the contact to edit: ");
+        String lastNameToEdit = scanner.nextLine();
+
+        Contact existingContact = findContactByName(firstNameToEdit, lastNameToEdit);
+
+        if (existingContact != null) {
+            System.out.println("Existing Contact Details:");
+            System.out.println(existingContact);
+            boolean updateMore = true;
+            while (updateMore) {
+            // Get updated information
+            System.out.println("Enter the information to update (address/city/state/zip/phone/email): ");
+            String infoToUpdate = scanner.nextLine();   
+            switch (infoToUpdate.toLowerCase()) {
+                case "address":
+                    System.out.print("Enter updated address: ");
+                    existingContact.setAddress(scanner.nextLine());
+                    break;
+                case "city":
+                    System.out.print("Enter updated city: ");
+                    existingContact.setCity(scanner.nextLine());
+                    break;
+                case "state":
+                    System.out.print("Enter updated state: ");
+                    existingContact.setState(scanner.nextLine());
+                    break;
+                case "zip":
+                    System.out.print("Enter updated ZIP Code: ");
+                    existingContact.setZip(scanner.nextLine());
+                    break;
+                case "phone":
+                    System.out.print("Enter updated phone number: ");
+                    existingContact.setPhone(scanner.nextLine());
+                    break;
+                case "email":
+                    System.out.print("Enter updated email: ");
+                    existingContact.setEmail(scanner.nextLine());
+                    break;
+                default:
+                    System.out.println("Invalid information type. No updates performed.");
+            }
+
+            System.out.print("Do you want to update anything more for this contact? (yes/no): ");
+            String updateMoreOption = scanner.nextLine();
+            updateMore = updateMoreOption.equalsIgnoreCase("yes");
+        }
+
+            System.out.println("Contact updated successfully");
+        } else {
+            System.out.println("Contact not found");
+        }
+    }
+    
+    
+	/*
+	 * @desc:Deletes a contact from the address book by first name and last name.
+	 * @params:Scanner
+	 * @retrun:none
+	 */
+    public void deleteContactByName(Scanner scanner) {
+        System.out.print("Enter the first name of the contact to delete: ");
+        String firstNameToDelete = scanner.nextLine();
+
+        System.out.print("Enter the last name of the contact to delete: ");
+        String lastNameToDelete = scanner.nextLine();
+
+        Contact contactToRemove = findContactByName(firstNameToDelete, lastNameToDelete);
+
+        if (contactToRemove != null) {
+            contacts.remove(contactToRemove);
+            System.out.println("Contact deleted successfully");
+        } else {
+            System.out.println("Contact not found. Deletion failed.");
+        }
+    }
+}
+
+
---
+++ src/com/bridgelabz/main/AddressBookMain.java
@@ -0,0 +1,80 @@
+package com.bridgelabz.main;
+
+import java.util.Scanner;
+
+/*@desc:Main class representing the Address Book program.*/
+public class AddressBookMain {
+	
+	
+	/*
+	 * @desc:Displays the contacts
+	 * @params:none
+	 * @retrun:none
+	 */
+	public static void main(String []args) {
+		System.out.println("WELCOME TO ADDRESS BOOK!!");
+		
+
+		        Address address = new Address();
+
+		        Scanner scanner = new Scanner(System.in);
+		        
+		        boolean addMoreContacts = true;
+
+		        while (addMoreContacts) {
+		        System.out.print("Enter first name: ");
+		        String fname = scanner.nextLine();
+
+		        System.out.print("Enter last name: ");
+		        String lname = scanner.nextLine();
+
+		        System.out.print("Enter address: ");
+		        String addressStr = scanner.nextLine();
+
+		        System.out.print("Enter city: ");
+		        String city = scanner.nextLine();
+
+		        System.out.print("Enter state: ");
+		        String state = scanner.nextLine();
+
+		        System.out.print("Enter zip: ");
+		        String zip = scanner.nextLine();
+
+		        System.out.print("Enter phone number: ");
+		        String phone = scanner.nextLine();
+
+		        System.out.print("Enter email: ");
+		        String email = scanner.nextLine();
+		        
+		        Contact newContact = new Contact(fname, lname, addressStr, city, state, zip, phone, email);
+		        address.addContact(newContact);
+
+	            System.out.print("Do you want to add another contact? (yes/no): ");
+	            String addAnother = scanner.nextLine();
+
+	            addMoreContacts = addAnother.equalsIgnoreCase("yes");
+		        }
+
+		        // Option to edit an existing contact
+		        System.out.print("Do you want to edit an existing contact? (yes/no): ");
+		        String editOption = scanner.nextLine();
+
+		        if (editOption.equalsIgnoreCase("yes")) {
+		            address.editExistingContact(scanner);
+		        }
+                
+		        
+		        // Option to delete an existing contact
+		        System.out.print("Do you want to delete an existing contact? (yes/no): ");
+		        String deleteOption = scanner.nextLine();
+
+		        if (deleteOption.equalsIgnoreCase("yes")) {
+		            address.deleteContactByName(scanner);
+		        }
+
+		        address.display();
+		        scanner.close();
+	}
+ 
+
+	}
